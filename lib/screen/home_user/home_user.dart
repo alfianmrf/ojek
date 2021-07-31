bfb551fb5069a1e17043cbae649d6d64f78287a9
ismail <ismailsl487@gmail.com>
2021-07-31 08:06:45 +0700

    [UI]:Home user add driver

--- lib/screen/home_user/home_user.dart
+++ lib/screen/home_user/home_user.dart
@@ -21,145 +21,284 @@ class _HomeUserScreenState extends State<HomeUserScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          width: double.infinity,
-          height: 300,
-          // color: Colors.red,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Color(0xFFFCCCBC),
-                    borderRadius: BorderRadius.only(
-                        // bottomLeft: Radius.circular(120),
-                        bottomRight: Radius.circular(80))),
-                width: double.infinity,
-                height: 250,
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                left: 0,
-                child: Container(
-                  height: 130,
-                  width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 40),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300,
+            // color: Colors.red,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 7,
-                        color: Colors.grey.withOpacity(0.30),
+                      color: Color(0xFFFCCCBC),
+                      borderRadius: BorderRadius.only(
+                          // bottomLeft: Radius.circular(120),
+                          bottomRight: Radius.circular(80))),
+                  width: double.infinity,
+                  height: 250,
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: Container(
+                    height: 130,
+                    width: double.infinity,
+                    margin: EdgeInsets.symmetric(horizontal: 40),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 7,
+                          color: Colors.grey.withOpacity(0.30),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
                       ),
-                    ],
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
                     ),
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        padding: EdgeInsets.all(10),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Color(0xFFFCCCBC), width: 1),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(10),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Color(0xFFFCCCBC), width: 1),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.add_location_alt,
+                                color: Colors.grey[400],
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 10),
+                                child: Text(
+                                  "Tambahkan lokasi kamu",
+                                  style: TextStyle(color: Colors.grey[400]),
+                                ),
+                              )
+                            ],
                           ),
                         ),
-                        child: Row(
+                        Container(
+                          width: double.infinity,
+                          height: 52,
+                          padding: EdgeInsets.all(8),
+                          child: TextButton(
+                            onPressed: () {},
+                            style: flatButtonStyle,
+                            child: Text("PILIH DESTINASI",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 14)),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 70, horizontal: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.account_circle_rounded,
+                        size: 70,
+                        color: Colors.white,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 17, 0, 0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(
-                              Icons.add_location_alt,
-                              color: Colors.grey[400],
+                            Text(
+                              "Hi, Customer",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black45,
+                                  fontSize: 17),
                             ),
-                            Container(
-                              margin: EdgeInsets.only(left: 10),
-                              child: Text(
-                                "Tambahkan lokasi kamu",
-                                style: TextStyle(color: Colors.grey[400]),
-                              ),
-                            )
+                            Text(
+                              "Nama Customer",
+                              style: TextStyle(
+                                  color: Colors.black45, fontSize: 12),
+                            ),
                           ],
                         ),
                       ),
-                      Container(
-                        width: double.infinity,
-                        height: 52,
-                        padding: EdgeInsets.all(8),
-                        child: TextButton(
-                          onPressed: () {},
-                          style: flatButtonStyle,
-                          child: Text("PILIH DESTINASI",
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 17, 0, 0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.logout_outlined,
+                              size: 30,
+                              color: Colors.black45,
+                            ),
+                            Text(
+                              "Log Out",
                               style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 14)),
+                                  color: Colors.black45,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
                         ),
                       )
                     ],
                   ),
                 ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 14, vertical: 20),
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black45),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
               ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 70, horizontal: 20),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Icon(
-                      Icons.account_circle_rounded,
-                      size: 70,
-                      color: Colors.white,
+                    Text(
+                      "Nama Customer",
+                      style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 17, 0, 0),
+                    Text(
+                      "Total : Rp 50.000",
+                      style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 10),
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        child: Image.network(
+                          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            "Hi, Customer",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black45,
-                                fontSize: 17),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Container(
+                              width: 230,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "Nama Driver : ",
+                                    style: TextStyle(
+                                        color: Colors.black54,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Jhon Doe",
+                                    style: TextStyle(
+                                      color: Colors.black45,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
-                          Text(
-                            "Nama Customer",
-                            style:
-                                TextStyle(color: Colors.black45, fontSize: 12),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Row(
+                              children: [
+                                Text(
+                                  "Plat Nomor : ",
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "L 27312 BK",
+                                  style: TextStyle(
+                                    color: Colors.black45,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Container(
+                              width: 230,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "Alamat Tujuan : ",
+                                    style: TextStyle(
+                                        color: Colors.black54,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Jawa Timur Park",
+                                    style: TextStyle(
+                                      color: Colors.black45,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ],
                       ),
                     ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 17, 0, 0),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.logout_outlined,
-                            size: 30,
-                            color: Colors.black45,
-                          ),
-                          Text(
-                            "Log Out",
-                            style: TextStyle(
-                                color: Colors.black45,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    )
                   ],
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
-    ));
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
