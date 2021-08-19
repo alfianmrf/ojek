98de6e5ea48a7f9d94fa5be4ec1685c52f438000
ismail <ismailsl487@gmail.com>
2021-08-19 13:50:25 +0700

    [Notification] : Notif With One Signal

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,18 @@ import 'package:ojek/model/map_model.dart';
 import 'package:ojek/model/model.dart';
 import 'package:ojek/model/searc_map.dart';
 import 'package:ojek/screen/splash_screen.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  OneSignal.shared.setAppId("302fc91f-1847-4650-9a8d-40d872fee45d");
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+  });
   // HttpOverrides.global = new MyHttpOverrides();
   runApp(MultiProvider(
     providers: [
@@ -24,9 +31,30 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  @override
+  void initState() {
+    super.initState();
+    OneSignal.shared.setNotificationWillShowInForegroundHandler(
+        (OSNotificationReceivedEvent event) {
+      // Will be called whenever a notification is received in foreground
+      // Display Notification, pass null param for not displaying the notification
+      event.complete(event.notification);
+    });
+
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      // Will be called whenever a notification is opened/button pressed.
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       builder: (BuildContext context, Widget? child) {
--- lib/screen/home/home_driver.dart
+++ lib/screen/home/home_driver.dart
@@ -9,6 +9,7 @@ import 'package:provider/provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import '../theme.dart';
 
